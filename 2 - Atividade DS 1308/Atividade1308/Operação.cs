0e4378a221aa1140f7a9b171f2f16da6b470791e
Pedro Campos Novais <62915258+pedroCN0@users.noreply.github.com>
2021-05-10 17:33:16 -0300

    Add files via upload

---
+++ 2 - Atividade DS 1308/Atividade1308/Operação.cs
@@ -0,0 +1,75 @@
+﻿   using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atividade1308
+{
+    class Operação
+    {
+        public int res;
+        public int a;
+        public int b;
+        public int escolha;
+
+        public string menu()
+        {
+            string i;
+            Console.WriteLine("Atividade Dia 13/08/2019 - Por: Bruno da Silva Costa - 2º DS");
+            Console.WriteLine("Digite o número referente a operação desejada:");
+            Console.WriteLine("");
+            Console.WriteLine("1: Soma");
+            Console.WriteLine("2: Subtração");
+            Console.WriteLine("3: Multiplicação");
+            Console.WriteLine("4: Divisão");
+            Console.WriteLine("");
+            Console.WriteLine("Digite 'Q' para sair.");
+            Console.WriteLine("");
+            i = Console.ReadLine();
+            return i;
+        }
+        public void obtern()
+        {
+            a = int.Parse(Console.ReadLine());
+            switch(escolha)
+            {
+                case 1:
+                    Console.WriteLine("+");
+                    break;
+                case 2:
+                    Console.WriteLine("-");
+                    break;
+                case 3:
+                    Console.WriteLine("*");
+                    break;
+                case 4:
+                    Console.WriteLine("/");
+                    break;
+            }
+            b = int.Parse(Console.ReadLine());
+        }
+        public void soma()
+        {
+            res = a + b;
+        }
+        public void subtracao()
+        {
+            res = a - b;
+        }
+        public void multiplicacao()
+        {
+            res = a * b;
+        }
+        public void divisao()
+        {
+            res = a / b;
+        }
+        public void resultado()
+        {
+            Console.WriteLine("Resposta é de: {0}",res);
+            Console.ReadKey();
+            Console.Clear();
+        }
+    }
+}
---
+++ 2 - Atividade DS 1308/Atividade1308/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atividade1308
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string i;
+            int ss;
+            Operação A;
+            A = new Operação();
+            i = "0";
+            while (i != "Q" && i != "q")
+            {
+                i = A.menu();
+                Console.Clear();
+                if (i != "Q" && i != "q")
+                {
+                    if (i != "Q" && i != "q")
+                    {
+                        ss = int.Parse(i);
+                        A.escolha = int.Parse(i);
+                        if (ss >= 1 && ss <= 4)
+                        {
+                            switch (i)
+                            {
+                                case "1":
+                                    Console.WriteLine("Operação escolhida: Soma.");
+                                    Console.WriteLine("Digite os 2 números da operação:");
+                                    A.obtern();
+                                    A.soma();
+                                    A.resultado();
+                                    break;
+                                case "2":
+                                    Console.WriteLine("Operação escolhida: Subtração.");
+                                    Console.WriteLine("Digite os 2 números da operação:");
+                                    A.obtern();
+                                    A.subtracao();
+                                    A.resultado();
+                                    break;
+                                case "3":
+                                    Console.WriteLine("Operação escolhida: Multiplicação.");
+                                    Console.WriteLine("Digite os 2 números da operação:");
+                                    A.obtern();
+                                    A.multiplicacao();
+                                    A.resultado();
+                                    break;
+                                case "4":
+                                    Console.WriteLine("Operação escolhida: Divisão.");
+                                    Console.WriteLine("Digite os 2 números da operação:");
+                                    A.obtern();
+                                    A.divisao();
+                                    A.resultado();
+                                    break;
+                            }
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("");
+                        Console.WriteLine("Erro: Número não detectado.");
+                        Console.WriteLine("");
+                        Console.ReadKey();
+                        Console.Clear();
+                    }
+                }
+            }
+        }
+    }
+}
